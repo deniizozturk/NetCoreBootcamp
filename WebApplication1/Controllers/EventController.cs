97fd4147137f8da700ef5cb98372d3f0e47d019d
Deniz Öztürk <deniizozturk@github.com>
2020-01-11 16:44:27 +0300

    Add project files.

---
+++ WebApplication1/Controllers/EventController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DTOs;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+
+        public EventController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add([FromBody]EventAddDTO model)
+        {
+            var result = _eventService.Add(model);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ WebApplication1/Services/EventService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DTOs;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public interface IEventService
+    {
+        string Add(EventAddDTO model);
+    }
+    public class EventService : IEventService
+    {
+        private static IList<Event> data = new List<Event>();
+        public string Add(EventAddDTO model)
+        {
+            Event entity = new Event
+            {
+                Id = Guid.NewGuid(),
+                CreatedAt = DateTime.UtcNow
+            };
+            entity.Name = model.Name;
+            entity.StartDate = model.StartDate;
+            entity.FinishDate = model.FinishDate;
+            entity.Address = model.Address;
+            entity.IsFree = model.IsFree;
+            entity.Price = model.Price;
+            entity.Subject = model.Subject;
+            entity.Desc = model.Desc;
+
+            data.Add(entity);
+
+            return ("İşlem Başarılı");
+
+        }
+    }
+    
+}
